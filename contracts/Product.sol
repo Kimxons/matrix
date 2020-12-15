cb2892613cbf34b8bc511e2bf79928b5b5b81129
kimxons <kitongameshack9@gmail.com>
2020-12-15 13:38:06 +0300

    hello truffle

--- contracts/Product.sol
+++ contracts/Product.sol
@@ -5,12 +5,36 @@ pragma experimental ABIEncoderV2;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Product is Ownable{
+import "./Registrations.sol";
+import "./Token/ERC20.sol";
+
+contract Product is Context, Ownable{
 	using SafeMath for uint; 
 
 	address payable contractOwner;
     bool private stopped; // for use in circuit breaker
 
+    bytes32 id;
+    address buyer;
+    address seller;
+    // mapping(address => uint256) supply;
+    Registrations private registrations;
+
+    address[] suppliers;
+
+    bytes32 deliveryDate;
+    bytes32 paymentDate;
+    uint256 orderAmount;
+    bytes32[50] orderDetails;
+
+    bool approvalSeller;
+
+    bool approvalBuyer;
+    bool orderCompletion;
+
+	/**
+	 * a struct acts as a loose bag of variables
+	 */
 	struct ProductSubmission{
 		string ProductHash;
 		string ProductInfo;
@@ -19,6 +43,71 @@ contract Product is Ownable{
 		uint256 BlockNumber;
 		uint isSet; 
 	}
+
+	// constructor(bytes32 _id) public {
+	// 	id = _id;
+	// }
+	
+	modifier checkUser(address _address) {
+        require(registrations.isRegistered(_address), "Access Denied");
+        _;
+    }
+    modifier onlyOnNum(uint256 num) {
+        require(num != 0, "Value not set");
+        _;
+    }
+
+    modifier onlyOnSuppliers {
+        require(suppliers.length != 0, "There are no suppliers");
+        _;
+    }
+    modifier onSellerTrue() {
+        require(approvalSeller == true, "Seller hasn't agreed");
+
+        _;
+    }
+    modifier onBuyerTrue() {
+        require(approvalSeller == true, "Seller hasn't agreed");
+
+        _;
+    }
+    modifier onlyOnSetAddress(address check) {
+        require(check != address(0), "Value not yet set");
+        _;
+    }
+
+    modifier onlyOnString(bytes32 str) {
+        require(str.length != 0, "There are no suppliers");
+        _;
+    }
+    modifier onStringNull(bytes32 str) {
+        require(str.length == 0, "Value has already been set");
+        _;
+    }
+    modifier onUserNull(address user) {
+        require(user == address(0), "Value already set");
+        _;
+    }
+
+    modifier onBoolNull(bool val) {
+        require(val == false, "Value already set");
+        _;
+    }
+    modifier onNumNull(uint256 num) {
+        require(num == 0, "Value has already been set");
+        _;
+    }
+
+    modifier checkOrderCompletion(bool _orderCompletion) {
+        require(_orderCompletion == true, "Order has to be completed");
+        _;
+    }
+    function setOrderAmt(uint256 _orderAmt) public onNumNull(orderAmount) {
+        orderAmount = _orderAmt;
+    }
+    function setOrderDetails(bytes32[50] memory _msg) public {
+        orderDetails = _msg;
+    }
 
 	/*
 	state variable to store a ProductSubmission struct for each possible product hash
@@ -114,4 +203,116 @@ contract Product is Ownable{
         // cast owner which is address to address payable
         selfdestruct(contractOwner);
     }
+
+    function setOrderCompletion(bool _orderCompletion)
+        public
+        onSellerTrue
+        onBuyerTrue
+        onlyOnOrderDetails
+        onBoolNull(orderCompletion)
+    {
+        orderCompletion = _orderCompletion;
+    }
+
+    function setBuyer(address _buyer)
+        public
+        onUserNull(buyer)
+        checkUser(_buyer)
+    {
+        buyer = _buyer;
+    }
+
+    function getBuyer() public view onlyOnSetAddress(buyer) returns (address) {
+        return buyer;
+    }
+    function getSeller()
+        public
+        view
+        onlyOnSetAddress(seller)
+        returns (address)
+    {
+        return seller;
+    }
+
+    function getSuppliers()
+        public
+        view
+        onlyOnSuppliers
+        onBuyerTrue
+        onSellerTrue
+        returns (address[] memory)
+    {
+        return suppliers;
+    }
+
+    function getDeliveryDate()
+        public
+        view
+        onlyOnString(deliveryDate)
+        returns (bytes32)
+    {
+        return deliveryDate;
+    }
+
+    function getPaymentDate()
+        public
+        view
+        onlyOnString(paymentDate)
+        returns (bytes32)
+    {
+        return paymentDate;
+    }
+
+    function getOrderAmt() public view returns (uint256) {
+        return orderAmount;
+    }
+    modifier onlyOnOrderDetails() {
+        require(orderDetails.length != 0, "Order Details not set");
+        _;
+    }
+    function getOrderDetails()
+        public
+        view
+        onlyOnOrderDetails
+        returns (bytes32[50] memory)
+    {
+        return orderDetails;
+    }
+
+    function getApprovalSeller() public view returns (bool) {
+        return approvalSeller;
+    }
+
+    function getBuyerApproval() public view returns (bool) {
+        return approvalBuyer;
+    }
+
+    function setSeller(address _seller)
+        public
+        onUserNull(seller)
+        checkUser(_seller)
+    {
+        seller = _seller;
+    }
+
+    function setApprovalSeller(bool _approvalSeller)
+        public
+        onlyOnString(deliveryDate)
+        onlyOnString(paymentDate)
+        onlyOnOrderDetails
+        onBoolNull(approvalSeller)
+        onlyOnNum(orderAmount)
+    {
+        approvalSeller = _approvalSeller;
+    }
+
+    function setApprovalBuyer(bool _approvalBuyer)
+        public
+        onSellerTrue
+        onBoolNull(approvalBuyer)
+    {
+        approvalBuyer = _approvalBuyer;
+    }
 }
+
+//TODO -- work on the product (buyer & seller) logistics
--- contracts/Token/ERC20.sol
+++ contracts/Token/ERC20.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
+/**
+ * Token.sol
+ * name - name of the token 
+ * symbol - symbol of the token
+ * decimals - number of significant decimals for the token 
+ * totalSupply - total supply of token (interface)
+ * balanceOf - Mapping to store number of tokens inside each address
+ * allowance - Mapping to store allowance of token transfer for each address
+ * 
+ */
+
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./TokenInterface.sol";
 
@@ -15,72 +26,53 @@ contract ERC20 is TokenInterface {
     uint8 public decimals;
     string public symbol;
 
-    constructor(
-        uint256 _initialAmount,
-        string memory _tokenName,
-        uint8 _decimalUnits,
-        string memory _tokenSymbol) public {
-        balances[msg.sender] = _initialAmount;
-        totalSupply = _initialAmount;
-        name = _tokenName;
-        decimals = _decimalUnits;
-        symbol = _tokenSymbol;
-    }
+    constructor(uint256 _initialAmount,
+                string memory _tokenName,
+                uint8 _decimalUnits,
+                string memory _tokenSymbol) public{
+                    balances[msg.sender] = _initialAmount;
+                    totalSupply = _initialAmount;
+                    name = _tokenName;
+                    decimals = _decimalUnits;
+                    symbol = _tokenSymbol;
+                }
 
     function transfer(address _to, uint256 _value)
-        public
-        override
-        returns (bool success)
-    {
-        require(balances[msg.sender] >= _value, "Insufficient Balance");
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
-        return true;
-    }
+        public override returns (bool success) {
+            require(balances[msg.sender] >= _value, "Insufficient Balance");
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            emit Transfer(msg.sender, _to, _value);
+            return true;
+        }
 
     function transferFrom(address _from, address _to, uint256 _value)public override returns (bool success){
         uint256 allowance = allowed[_from][msg.sender];
-        require(
-            balances[_from] >= _value && allowance >= _value,
-            "Insufficient Balance"
-        );
+        require(balances[_from] >= _value && allowance >= _value,"Insufficient Balance");
+
         balances[_to] = balances[_to].add(_value);
         balances[_from] = balances[_from].sub(_value);
+        
         if (allowance < MAX_UINT256) {
             allowed[_from][msg.sender] -= _value;
         }
+
         emit Transfer(_from, _to, _value); 
         return true;
     }
 
-    function balanceOf(address _owner)
-        public
-        view
-        override
-        returns (uint256 balance)
-    {
-        return balances[_owner];
-    }
+    function balanceOf(address _owner) public view override returns (uint256 balance){
+            return balances[_owner];
+        }
 
-    function approve(address _spender, uint256 _value)
-        public
-        override
-        returns (bool success)
-    {
+    function approve(address _spender, uint256 _value)public override returns (bool success){
         allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
+        emit Approval(msg.sender, _spender, _value); 
         return true;
     }
 
-    function allowance(address _owner, address _spender)
-        public
-        view
-        override
-        returns (uint256 remaining)
-    {
+    function allowance(address _owner, address _spender) public view override returns (uint256 remaining){
         return allowed[_owner][_spender];
     }
-
 
 }
